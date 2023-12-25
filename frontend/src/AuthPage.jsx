2a566464fdb52e29fe99c8f4a009ebeccee082b4
graceco22 <132722611+graceco22@users.noreply.github.com>
2023-12-25 17:42:06 -0500

    changed post

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios
-      .post("http://localhost:3001/authenticate", { username: value })
+      .post("chat-pearl-ten.vercel.app", { username: value })
       .then((r) => props.onAuth({ ...r.data, secret: value }))
       .catch((e) => console.log("error", e));
   };
